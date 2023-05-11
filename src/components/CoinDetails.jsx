ce11c385fadeb002a266f23f3704d3e444287f8d
Dipanshugarg65 <118041289+Dipanshugarg65@users.noreply.github.com>
2023-05-11 21:44:16 +0530

    Add files via upload

---
+++ src/components/CoinDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoinDetails = () => {
+  return (
+    <div>
+      details
+    </div>
+  )
+}
+
+export default CoinDetails
---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Exchanges = () => {
+  return (
+    <div>
+      Exchangesnager
+    </div>
+  )
+}
+
+export default Exchanges
